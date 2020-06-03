d3544f545164f2f36173e65d37a665426c25104d
tina <justi9326@gmail.com>
2020-06-03 17:14:49 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//alert('고영희입니까?');
+/*console.log('gocat');
+
+const a = 4;
+
+let b = 5;
+
+function sayHello(name, age){
+    console.log(`내 이름은 ${name} 입니다. 나이는 ${age} 살 압니다. `)
+}
+
+sayHello('고영희', 20);
+*/
+
+
